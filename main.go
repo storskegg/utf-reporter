f2fec61e08619d040dfe3ac85eb56bb0a478a50c
Liam Conrad <liam@storskegg.org>
2022-03-01 19:31:49 -0600

    Better formatting, incl rune width and reference url

--- main.go
+++ main.go
@@ -2,51 +2,92 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"os"
+	"path"
+	"strings"
 
+	"github.com/chzyer/readline/runes"
 	"github.com/fatih/color"
 	"github.com/rodaine/table"
 	rune2 "github.com/storskegg/utf-reporter/rune"
 )
 
 func printUsage() {
-	fmt.Println("utf-reporter is intended to be used with piped input. File arguments are not yet supported.")
-	fmt.Println("Example Usage: cat somefile.csv | utf-reporter")
-
-	//flag.VisitAll(func(f *flag.Flag) {
-	//	fmt.Printf("  %s\t%s\n", f.Name, f.Usage)
-	//})
+	fmt.Println("Examples:")
+	fmt.Println("  cat somefile.csv | utf-reporter")
+	fmt.Println("  utf-reporter -f <path to text file>")
+	fmt.Println()
+	fmt.Println("Usage of utf-reporter:")
+	flag.VisitAll(func(f *flag.Flag) {
+		fmt.Printf("  %s\t%s\n", f.Name, f.Usage)
+	})
 }
 
 func main() {
-	// Our Flags
-	//var posix, all bool
-	//
-	//flag.BoolVar(&posix, "posix", false, "Use POSIX regex.  Defaults to PCRE")
-	//flag.BoolVar(&all, "all", false, "Return all matches. Defaults to the first match")
-	//
-	//flag.Parse()
-	//
-	//if flag.NArg() < 1 {
-	//	printUsage()
-	//	return
-	//}
+	var err error
+	var info os.FileInfo
 
-	// Check stdin for piped input
-	info, err := os.Stdin.Stat()
-	if err != nil {
-		panic(err)
+	// Our Flags
+	var flagFile string
+	var flagNoColor bool
+
+	flag.StringVar(&flagFile, "f", "", "Input text file")
+	flag.BoolVar(&flagNoColor, "no-color", false, "Disable color output")
+	flag.Parse()
+	if flag.NArg() > 0 {
+		if !(flag.NArg() == 1 && flag.Arg(0) == "-") {
+			printUsage()
+			return
+		}
 	}
 
-	if info.Mode()&os.ModeNamedPipe == 0 {
-		printUsage()
-		return
+	if flagNoColor {
+		color.NoColor = true
+	}
+
+	var input *os.File
+
+	// this could be cleaned up a bit
+	if flagFile == "" {
+		input = os.Stdin
+		info, err = input.Stat()
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		flagFile = path.Join(".", flagFile)
+		info, err = os.Stat(flagFile)
+		if err != nil {
+			if err == os.ErrNotExist {
+				fmt.Printf("The path '%s' does not exist.\n", flagFile)
+				return
+			}
+			panic(err)
+		}
+		input, err = os.Open(flagFile)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	// Check stdin for piped input
+	if flagFile == "" {
+		if info.Mode()&os.ModeNamedPipe == 0 {
+			printUsage()
+			return
+		}
+	} else {
+		if !info.Mode().IsRegular() {
+			printUsage()
+			return
+		}
 	}
 
 	// Capture piped input, capturing runes
-	reader := bufio.NewReader(os.Stdin)
+	reader := bufio.NewReader(input)
 	captured := make(rune2.SpecialRunesLines)
 
 	lineNum := 0
@@ -69,23 +110,33 @@ func main() {
 		captured[lineNum] = sr
 	}
 
-	headerFmt := color.New(color.FgGreen, color.Underline).SprintfFunc()
+	headerFmt := color.New(color.FgBlue, color.Underline).SprintfFunc()
 	columnFmt := color.New(color.FgYellow).SprintfFunc()
 
 	if len(captured) == 0 {
 		fmt.Println("No non-standard characters found.")
 	} else {
 		for _, lineNum := range captured.SortedColumns() {
-			fmt.Printf("===[ Line %d ]==========================================\n", lineNum)
-			tbl := table.New("Column", "Rune", "Hex", "Type")
-			tbl.WithHeaderFormatter(headerFmt).WithFirstColumnFormatter(columnFmt)
+			tbl := table.New("Column", "Rune", "Hex", "Type", "Width", "Reference")
+			tbl.WithHeaderFormatter(headerFmt).WithFirstColumnFormatter(columnFmt).WithPadding(3)
+			tbl.WithWidthFunc(func(s string) int {
+				return runes.WidthAll([]rune(s))
+			})
 
 			sr = captured[lineNum]
 			for _, colNum = range sr.SortedColumns() {
 				rr = sr[colNum]
-				tbl.AddRow(colNum, fmt.Sprintf("'%c'", rr), "0x"+rr.CharCodeWithPadding(), rr.RuneType())
+				tbl.AddRow(
+					colNum,
+					fmt.Sprintf("'%c'", rr),
+					"0x"+rr.CharCodeWithPadding(),
+					rr.RuneType(),
+					runes.Width(rune(rr)),
+					fmt.Sprintf("https://www.compart.com/en/unicode/U+%s", strings.ToUpper(rr.CharCodeWithPadding())),
+				)
 				//fmt.Printf(" Found: 0x%s ('%c') at column %d of type %s\n", rr.CharCodeWithPadding(), rr, colNum, rr.RuneType())
 			}
+			color.HiWhite("Line %d:", lineNum)
 			tbl.Print()
 			fmt.Println()
 		}
